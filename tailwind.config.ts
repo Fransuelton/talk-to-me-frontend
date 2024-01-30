fb18558fbca66d2c69a03c9443d48e331a323453
Fransuelton <elton6103@gmail.com>
2024-01-30 10:15:05 -0300

    style: add new colors to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,14 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'gray-1000': '#2E2E2E',
+        'gray-950': '#4E4E4E',
+        black: '#0F0F0F',
+        primary: '#81E6D9',
+        secondary: '#212121',
+        'gray-900': '#2C2C2C'
+      }
     },
   },
   plugins: [],

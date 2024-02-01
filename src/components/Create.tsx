9b39801b92f9bee5937b494fe90718033e601321
Fransuelton <elton6103@gmail.com>
2024-02-01 16:45:33 -0300

    feat: Add client-side validation and room creation functionality

--- src/components/Create.tsx
+++ src/components/Create.tsx
@@ -1,15 +1,35 @@
-import { useRef } from "react";
+"use client";
+import { FormEvent, useRef } from "react";
 import Button from "./Button";
 import { Input } from "./Input";
+import { useRouter } from "next/navigation";
 
 export default function CreateRoom() {
   const name = useRef<HTMLInputElement>(null);
+  const router = useRouter();
+
+  const handleCreateRoom = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (name.current && name.current.value !== "") {
+      sessionStorage.setItem("username", name.current.value);
+      const roomId = generateRandomString();
+      console.log("🚀 ~ handleCreateRoom ~ roomId:", roomId);
+      router.push(`/room/${roomId}`);
+    }
+  };
+
+  function generateRandomString() {
+    const randomString = Math.random().toString(36).substring(2, 7);
+    return randomString;
+  }
 
   return (
     <>
-      <Input placeholder="Seu nome" type="text" ref={name} />
+      <form onSubmit={(e) => handleCreateRoom(e)} className="space-y-8">
+        <Input placeholder="Seu nome" type="text" ref={name} />
 
-      <Button title="Entrar" type="button" />
+        <Button title="Entrar" type="submit" />
+      </form>
     </>
   );
 }

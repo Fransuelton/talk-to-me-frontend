0ad9f33ae43d8c3c8af75a541199649221f44f9f
Fransuelton <elton6103@gmail.com>
2024-02-01 16:46:09 -0300

    feat: Add video streaming functionality to Room component

--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -1,8 +1,33 @@
+"use client";
 import Chat from "@/components/Chat";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import { SocketContext } from "@/contexts/SocketContext";
+import { useContext, useEffect, useRef } from "react";
 
 export default function Room({ params }: { params: { id: string } }) {
+  const { socket } = useContext(SocketContext);
+  const localStream = useRef<HTMLVideoElement>(null);
+  useEffect(() => {
+    socket?.on("connect", async () => {
+      console.log("conectado");
+      socket?.emit("subscribe", {
+        roomId: params.id,
+        socketId: socket.id,
+      });
+      await initCamera();
+    });
+  }, [socket]);
+
+  const initCamera = async () => {
+    const video = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    });
+    console.log("🚀 ~ initCamera ~ video:", video);
+    if (localStream.current) localStream.current.srcObject = video;
+  };
+
   return (
     <div className="h-mas screen">
       <Header />
@@ -10,11 +35,15 @@ export default function Room({ params }: { params: { id: string } }) {
         <div className="md:w-[85%] w-full m-3 ">
           <div className="grid md:grid-cols-2 grid-cols-1 gap-8">
             <div className="bg-gray-950 w-full rounded-md h-full p-2 relative ">
-              <video className="h-full w-full"></video>
+              <video
+                className="h-full w-full"
+                autoPlay
+                ref={localStream}
+              ></video>
               <span className="absolute bottom-3">Alexia Kattah</span>
             </div>
             <div className="bg-gray-950 w-full rounded-md h-full p-2 relative ">
-              <video className="h-full w-full"></video>
+              <video className="h-full w-full" />
               <span className="absolute bottom-3">Alexia Kattah</span>
             </div>
             <div className="bg-gray-950 w-full rounded-md h-full p-2 relative ">
@@ -27,7 +56,7 @@ export default function Room({ params }: { params: { id: string } }) {
             </div>
           </div>
         </div>
-        <Chat />
+        <Chat roomId={params.id} />
       </div>
       <Footer />
     </div>
